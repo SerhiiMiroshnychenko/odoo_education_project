c12df54583d76654db9d04b1e950dacc91f8fc69
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-08-03 19:40:17 +0300

    #main: [REF] Refactoring model's fields

--- extra_addons/edu_module/models/demo_model.py
+++ extra_addons/edu_module/models/demo_model.py
@@ -5,12 +5,13 @@ class DemoModel(models.Model):
     _name = 'demo.model'
     _description = 'Model for demo object'
 
-    name = fields.Char(string='Name', required=True, size=15)
-    price = fields.Monetary(string='Price', currency_field='currency_id')
-    quantity = fields.Float(string='Quantity')
-    total_bill = fields.Monetary(string='Total bill',
-                                 currency_field='currency_id',
-                                 compute='_compute_total_bill')
+    name = fields.Char(required=True, size=15)
+    price = fields.Monetary(currency_field='currency_id')
+    quantity = fields.Float()
+    total_bill = fields.Monetary(
+        currency_field='currency_id',
+        compute='_compute_total_bill'
+    )
     currency_id = fields.Many2one('res.currency')
 
     @api.depends("price", "quantity")
