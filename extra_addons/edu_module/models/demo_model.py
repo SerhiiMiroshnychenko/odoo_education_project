88c505193e9ccc979b6f116a49a2499a5e2160ad
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-08-03 12:34:36 +0300

    #main: [DEV] Added fields price, total_bill, currency_id and views for its.

--- extra_addons/edu_module/models/demo_model.py
+++ extra_addons/edu_module/models/demo_model.py
@@ -1,9 +1,19 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class DemoModel(models.Model):
     _name = 'demo.model'
     _description = 'Model for demo object'
 
-    name = fields.Char(string='Name', required=True)
-    quantity = fields.Float(string='Quantity', default=0.0)
+    name = fields.Char(string='Name', required=True, size=15)
+    price = fields.Monetary(string='Price', currency_field='currency_id')
+    quantity = fields.Float(string='Quantity')
+    total_bill = fields.Monetary(string='Total bill',
+                                 currency_field='currency_id',
+                                 compute='_compute_total_bill')
+    currency_id = fields.Many2one('res.currency')
+
+    @api.depends("price", "quantity")
+    def _compute_total_bill(self):
+        for product in self:
+            product.total_bill = product.price * product.quantity
